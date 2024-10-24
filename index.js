4ca7a76441d5147c822589e51a9fdd99ab54ef27
siva <siva@sivas-MacBook-Air.local>
2024-10-24 16:15:40 +0530

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from 'dotenv';
 import route from "./routes/userRoute.js";
+import cors from 'cors';
 
 const app = express()
 
