652cf4971197befbf093f690a45f8eff4dadc571
siva <siva@sivas-MacBook-Air.local>
2024-10-24 17:10:06 +0530

    add delete

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { fetch ,create} from "../controller/userController.js";
+import { fetch ,create,update,remove} from "../controller/userController.js";
 
 const route = express.Router()
 
 route.post('/create',create)
 
 route.get('/fetch',fetch)
+route.put('update/:id', update); // Update user by ID
+route.delete('delete/:id', remove); // Delete user by ID
+
 
 export default route;
